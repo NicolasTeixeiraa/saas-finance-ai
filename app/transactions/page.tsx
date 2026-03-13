19295ec66c6326635d410cf9eb211c4bce5abb4d
Nicolas Teixeira <nicolas130802@gmail.com>
2026-03-13 15:58:35 -0300

    feat: ensure transaction can be updated

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
-import AddTransactionButton from "../_components/add-transaction-button";
+import AddTransaction from "../_components/add-transaction-button";
 
 const TransactionsPage = async () => {
   // acessar as transações do banco de dados
@@ -12,7 +12,7 @@ const TransactionsPage = async () => {
       {/* Título e Botão*/}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactionButton />
+        <AddTransaction />
       </div>
       <DataTable columns={transactionColumns} data={transactions} />
     </div>
